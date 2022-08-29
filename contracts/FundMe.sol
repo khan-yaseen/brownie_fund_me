ef2e79eda1e064af8f793931532ebf78f7084c45
khan-yaseen <yaseenkhan61@yahoo.com>
2022-08-29 20:18:35 +0530

    first commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+    // using SafeMathChainink for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+    address public owner;
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        // $50
+        uint256 minimumUSD = 0.5 * 10**18;
+        require(
+            getConversionRate(msg.value) >= minimumUSD,
+            "You need to spend more ETH!"
+        );
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+        // what is the ETH -> USD conversion rates
+    }
+
+    function getVersion() public view returns (uint256) {
+        // initialising priceFeed in the constructor
+        // AggregatorV3Interface priceFeed = AggregatorV3Interface(
+        //     0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        // );
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns (uint256) {
+        // initialising priceFeed in the constructor
+        // AggregatorV3Interface priceFeed = AggregatorV3Interface(
+        //     0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        // );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        return uint256(answer * 10000000000);
+    }
+
+    // 1000000000
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        return ethAmountInUsd;
+        // 1842163956330.000000000000000000
+        // 0.000001842163956330
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function withdraw() public payable onlyOwner {
+        // require(msg.sender == owner);
+        payable(msg.sender).transfer(address(this).balance);
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+    }
+
+    function checkBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        // minimumUSD
+        uint256 minimumUSD = 50 * 10**18;
+        uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        // return (minimumUSD * precision) / price;
+        // We fixed a rounding error found in the video by adding one!
+        return ((minimumUSD * precision) / price) + 1;
+    }
+}
